34e931e4f94df2cd45de302151627b53c2721101
Ricky Bacon <rickyb@criticalmass.com>
2020-12-12 00:46:09 -0500

    Redoing index page

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -6,30 +6,43 @@ import { onMount } from 'svelte'
 const controller = new ScrollMagic.Controller();
 
 onMount(() => {
+  /*
   const intro = new ScrollMagic.Scene({
-    duration: 500
+    duration: 100
   })
   .setPin('.intro')
   .addTo(controller);
 
+  const info = new ScrollMagic.Scene({
+    duration: 200
+  })
+  .setPin('.info')
+  .addTo(controller);
+
   const whatsYourVibe = new ScrollMagic.Scene({
-    duration: 1000
+    duration: 1500
   })
   .setPin('.whats-your-vibe')
   .addTo(controller);
 
   const howItWorks = new ScrollMagic.Scene({
-    duration: 1500
+    duration: 2000
   })
   .setPin('.how-it-works')
   .addTo(controller);
+  */
 });
 </script>
 
 <div class="main">
   <section class="intro">
   	<div class="container">
-      <h1><span class="vibe-logo">vibe</span> is the next generation of meditation and mindfulness experiences.</h1>
+      <div class="vibe-logo">vibe</div>
+      <h1>The next generation of meditation and mindfulness experiences.</h1>
+    </div>
+  </section>
+  <section class="info">
+    <div class="container">
       <div>With <span class="vibe-logo">vibe</span> you define the set - the mindset you want to bring to your experience.</div>
       <div><span class="vibe-logo">vibe</span> provides the setting - immersing you in a virtual environment that adapts to your set.</div>
       <div>set + setting</div>
@@ -50,11 +63,9 @@ onMount(() => {
   <section class="how-it-works">
   	<div class="container">
       <h2 class="center">How it works</h2>
-      <ul>
-        <li><span class="vibe-logo">vibe <span class="default-fonts">| Listen</span></span> creates generative soundscapes for your set to build your settings.</li>
-        <li><span class="vibe-logo">vibe <span class="default-fonts">| Watch</span></span> blends generative soundscapes with generative videoscapes to enchance your experience.</li>
-        <li><span class="vibe-logo">vibe <span class="default-fonts">| Immerse</span></span> uses the power of eXtended Reality to create immersive extra-spatial experiences.</li>
-      </ul>
+      <div> <span class="vibe-logo">vibe</span> <span class="default-fonts">| Listen</span> creates generative soundscapes for your set to build your settings.</div>
+      <div> <span class="vibe-logo">vibe</span> <span class="default-fonts">| Watch</span> blends generative soundscapes with generative videoscapes to enchance your experience.</div>
+      <div> <span class="vibe-logo">vibe</span> <span class="default-fonts">| Immerse</span> uses the power of eXtended Reality to create immersive extra-spatial experiences.</div>
       <h3 class="center">Experience your higher self.</h3>
       <h3 class="center">Catch your <span class="vibe-logo">vibe</span>.</h3>
     </div>
@@ -67,7 +78,27 @@ onMount(() => {
   :global(body) {
   	background-color: $black;
   	color: $white;
-  	padding: 0% 10%;
+  	padding: 0% 0%;
+  }
+
+  .center {
+    text-align: center;
+  }
+
+  .default-fonts {
+    font-family: $default-font-family !important;
+    font-size: $text-4xl;
+  }
+
+  .vibe-logo {
+    text-align: bottom;
+    font-family: AristaProAlternateLight;
+    font-size: 2.65rem;
+    color: $white;
+  }
+
+  section {
+    
   }
 
   div.main {
@@ -75,42 +106,57 @@ onMount(() => {
   	font-weight: 200;
   	text-align: center;
   	width: 100%;
+    padding: 0px;
+    margin: 0px;
+
 
   	h1, h2, h3 {
   	  font-weight: 200;
+      padding: 0px;
+      margin: 0px;
   	}
 
-  	section {
-  	  height: 100vh;
+  	section.intro {
+      width: 100%;
+      height: 100vh;
+      background-image: linear-gradient(45deg, $indigo-700, $red-600);
 
   	  .container {
         position: relative;
         top: 50%;
-	    transform: translateY(-50%);
+	      transform: translateY(-50%);
   	  }
+
+      .vibe-logo {
+        font-size: 10rem;
+        color: $white !important;
+        /* background-image: linear-gradient(45deg, $indigo-700, $red-600); */
+        /* @include prepareTextForGradient; */
+      }
     }
 
-    .how-it-works {
-      text-align: left;
+    section.info {
+        padding: 20rem 0;
+
+        .vibe-logo {
+          background-image: linear-gradient(45deg, $indigo-700, $fuchsia-600);
+
+          @include prepareTextForGradient;
+        }
+    }
+
+    section.whats-your-vibe {
+      
+    }
+
+    section.how-it-works {
+      
     }
   }
 
-  .center {
-  	text-align: center;
-  }
+  .intro {
 
-  .default-fonts {
-  	font-family: $default-font-family !important;
-  	font-size: $text-4xl;
-  }
-
-  .vibe-logo {
-  	text-align: bottom;
-  	font-family: AristaProAlternateLight;
-  	font-size: 2.65rem;
-
-  	background-image: linear-gradient(45deg, $indigo-700, $fuchsia-600);
-
-  	@include prepareTextForGradient;
+    h1 {
+    }
   }
 </style>
